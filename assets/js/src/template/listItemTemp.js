6c366ed89ba20c1239ac8d6401ef75e994f85e9f
basel8 <basel84mn@gmail.com>
2019-02-08 11:23:13 +0100

    added modal tooltip, description textarea

--- assets/js/src/template/listItemTemp.js
+++ assets/js/src/template/listItemTemp.js
@@ -2,15 +2,23 @@ export function listItemTemp(data) {
   let item = `<li class="list-group-item mt-1 ${data.id}" draggable="true">
                <span class="list-item-name">${data.text}</span>
                <div class="d-block mt-2">
-                 <span class="badge badge-pill badge-secondary">${data.user}</span>
-                 <span class="badge badge-pill badge-success">${data.date}</span>
+                 <span class="badge badge-pill badge-secondary">${
+                   data.user
+                 }</span>
+                 <span class="badge badge-pill badge-success">${
+                   data.date
+                 }</span>
                </div>
                <div class="dropdown leftDrop dropright">
                  <span data-toggle="dropdown"><i class="fas fa-pen p-1"></i></span>
                  <div class="dropdown-menu todoDropdownHolder border-0">
                    <div class="w-100">
-                     <span class="dropdown-item rounded" data-toggle="modal" href="#" data-target="#${data.id}">Edit Labels</span>
-                     <span class="dropdown-item rounded removeCard" href="#" data-set="${data.id}">Remove Item</span>
+                     <span class="dropdown-item rounded" data-toggle="modal" href="#" data-target="#${
+                       data.id
+                     }">Edit Labels</span>
+                     <span class="dropdown-item rounded removeCard" href="#" data-set="${
+                       data.id
+                     }">Remove Item</span>
                    </div>
                  </div>
                </div>
@@ -20,13 +28,19 @@ export function listItemTemp(data) {
                 <div class="modal-content">
                   <div class="modal-header">
                     <h4 class="modal-title">${data.text}</h4>
-                    <input type="text" value="${data.text}" class="modal-header-input">
+                    <input type="text" value="${
+                      data.text
+                    }" class="modal-header-input">
+                    <div class="modal-header-tooltip">Click to edit</div>
                   </div>
                   <div class="modal-body">
-                    <input type="text" value="${data.itemDescription}" class="modal-body-input">${data.itemDescription}
+                    <textarea class="form-control modal-body-input" id="exampleFormControlTextarea1" rows="3">${
+                      data.itemDescription
+                    }</textarea>
+                    <div class="modal-header-tooltip">Click to edit</div>
                   </div>
                   <div class="modal-footer">
-                    <button type="button" class="btn btn-info editCardDone" data-dismiss="modal">Done</button>
+                    <button type="button" class="btn btn-info editCardDone" data-dismiss="modal">Change</button>
                   </div>
                 </div>
               </div>
